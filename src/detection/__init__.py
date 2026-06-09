9734163af8ad210cbec1bb09c4cc7cd980508f2c
Parkdev22222 <164600467+Parkdev22222@users.noreply.github.com>
2026-06-09 12:14:24 +0900

    Update __init__.py

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,5 +1,5 @@
 from .image_loader import ImageMeta, LoadedImage, load_metadata_index, iter_images, pixel_to_geo
-from .sam2_detector import SAM3Detector, DetectionResult
+from .sam3_detector import SAM3Detector, DetectionResult
 
 __all__ = [
     "ImageMeta", "LoadedImage", "load_metadata_index", "iter_images", "pixel_to_geo",
